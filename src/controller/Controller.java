d028641907f8f681d01e8fa776bd275a741c1105
Olya Schulpina <olgrshulpina@edu.hse.ru>
2026-07-08 21:50:44 +0500

    Adding new fields in Habit class, adding data and test the controller with them

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,23 @@
 package controller;
 
+import model.Habit;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Controller {
     public static void main(String[] args) {
+        Habit habit1 = new Habit();
+        habit1.setName("Doing Java Code");
+        habit1.setStart(LocalDate.of(2026,7,6));
+        Habit habit2 = new Habit();
+        habit2.setName("Swimming session");
+        habit2.setStart(LocalDate.of(2026,7,4));
+        List<Habit> habits = new ArrayList<>();
+        habits.add(habit1);
+        habits.add(habit2);
 
         while (true){
             System.out.println("===Habit Tracker Menu===");
@@ -14,10 +28,18 @@ public class Controller {
             int switcher = scanner.nextInt();
             switch (switcher){
                 case 1:
-                    System.out.println("There will be all habits");
+                    for (int i = 0; i<habits.toArray().length; i++){
+                        System.out.println(i+1+". "+habits.get(i).getName());
+                    }
                     break;
                 case 2:
-                    System.out.println("It will habit's page");
+                    System.out.println("Choose a habit");
+                    for (int i = 0; i<habits.toArray().length; i++){
+                        System.out.println(i+1+". "+habits.get(i).getName());
+                    }
+                    int chosenhabit = scanner.nextInt();
+                    habits.get(chosenhabit-1).setStatus(true);
+                    System.out.println("Habit is ticked. Current track is "+habits.get(chosenhabit-1).getTrack());
                     break;
                 case 3:
                     break; /* нужно подумать как прервать выполнение программы*/
--- src/model/Habit.java
+++ src/model/Habit.java
@@ -1,11 +1,14 @@
 package model;
 
-class Habit{
-    int id;
-    String name;
-    boolean status;
-    int track;
-    /* как-то добавить дату */
+import java.time.LocalDate;
+
+public class Habit{
+    private int id;
+    private String name;
+    private boolean status = false;
+    /* как-то сделать статус для каждого дня в отдельности*/
+    private int track = 0;
+    private LocalDate start = LocalDate.now();
 
     public int getId() {
         return id;
@@ -15,7 +18,20 @@ class Habit{
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setStatus(boolean status) {
         this.status = status;
+    }
+    public int getTrack(){
+        return LocalDate.now().compareTo(start)+1;
+    }
+    public LocalDate getStart(){
+        return start;
+    }
+    public void setStart(LocalDate start){
+        this.start = start;
     }
 }
